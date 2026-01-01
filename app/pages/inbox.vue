64bac68f75cfd422ee05a6a378110e04ee536306
Linus Fischer <leicraft@leicraftmc.de>
2026-01-01 21:21:57 +0000

    updated inbox ui

--- app/pages/inbox.vue
+++ app/pages/inbox.vue
@@ -1,3 +1,23 @@
+<script setup lang="ts">
+
+// mock data
+const mails: Mail[] = [
+    // add mock mails here
+];
+
+</script>
+
 <template>
-    
+    <UDashboardPanel>
+        <template #header>
+            <DashboardPageHeader
+                title="Inbox"
+                icon="i-lucide-inbox"
+            />
+        </template>
+
+        <template #body>
+            <DashboardPageBody></DashboardPageBody>
+        </template>
+    </UDashboardPanel>
 </template>
