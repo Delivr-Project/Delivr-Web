ec32f90b6f0c7ee300b5abfb7607438a43bcd61b
Linus Fischer <leicraft@leicraftmc.de>
2026-01-27 22:54:36 +0000

    update

--- app/layouts/auth.vue
+++ app/layouts/auth.vue
@@ -3,33 +3,45 @@
 </script>
 
 <template>
-    <UMain class="main-content">
-        <div class="app-layout">
-            <!-- Main Content -->
-            <UPageCard class="w-full max-w-md border-slate-800">
-                <slot />
-            </UPageCard>
 
-        </div>
-    </UMain>
+    <div class="app-layout main-bg-color">
+        
+        <!-- Main Content -->
+        <UMain class="main-content">
+            <div class="auth-wrapper flex min-h-[calc(100vh-0px)] items-center justify-center">
+                <UPageCard class="w-full max-w-md border-slate-800">
+                    <slot />
+                </UPageCard>
 
-    <UFooter class="mt-8">
-        <div class="text-sm text-slate-500 text-center">
-            &copy; {{ new Date().getFullYear() }} Delivr Project. All rights reserved.
-        </div>
-    </UFooter>
+            </div>
+        </UMain>
+
+        <!-- Footer -->
+        <UFooter class="mt-8 bg-neutral-900">
+            <div class="text-sm text-slate-500 text-center">
+                &copy; {{ new Date().getFullYear() }} Delivr Project. All rights reserved.
+            </div>
+        </UFooter>
+
+    </div>
+
 </template>
 
 <style scoped>
+
 .app-layout {
-    display: flex;
-    align-items: center;
-    justify-content: center;
     min-height: 100vh;
-    margin-top: -2rem;
+    display: flex;
+    flex-direction: column;
 }
 
 .main-content {
     flex: 1;
 }
+
+.auth-wrapper {
+    margin-top: -2rem;
+}
+
+
 </style>
